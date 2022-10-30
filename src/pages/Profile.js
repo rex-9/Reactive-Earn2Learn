0ed1f021f9e7bee2f9ec49d21a0d4ed760acd3e7
Htet Naing <htetnaing0814@gmail.com>
2022-10-30 20:06:52 +0630

    added base user data

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,16 @@
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 const Profile = () => {
   const { id } = useParams();
+  const learners = useSelector((state) => state.learners);
+  const learner = learners.filter((learner) => learner.id === parseInt(id, 10))[0];
 
   return (
     <>
       <div>Profile</div>
-      <div>{ id }</div>
+      <div>{id}</div>
+      <div>{learner.username}</div>
     </>
   );
 };
