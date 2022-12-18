9e147260329c5c5ceffc6d1c8fee9be937454ade
Htet Naing <htetnaing0814@gmail.com>
2022-12-18 23:36:29 +0630

    Basic layout of social column

--- src/components/admin/Users.jsx
+++ src/components/admin/Users.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchLearners, sortLearners } from "../../redux/reducers/learnerXer";
 import EditLearner from "../learner/EditLearner";
-import avatar from "../../assets/avatar.jpg";
 import Alert from "../Alert";
+import assets from "../../assets/assets";
 
 const Users = () => {
   const [edit, setEdit] = useState(false);
@@ -61,6 +62,7 @@ const Users = () => {
             <th className="table-head"><button type="button" onClick={() => sort("fullname")}>Full Name</button></th>
             <th className="table-head"><button type="button" onClick={() => sort("goal")}>Goal</button></th>
             <th className="table-head"><button type="button" onClick={() => sort("email")}>Email</button></th>
+            <th className="table-head"><button type="button" onClick={() => sort("email")}>Social</button></th>
             <th className="table-head"><button type="button" onClick={() => sort("city")}>City</button></th>
             <th className="table-head"><button type="button" onClick={() => sort("phone")}>Phone</button></th>
             <th className="table-head"><button type="button" onClick={() => sort("bio")}>Bio</button></th>
@@ -76,11 +78,17 @@ const Users = () => {
             learners.map((learner) => (
               <tr key={learner.id} className="table-stripe font-semibold">
                 <td className="table-data">{learner.id}</td>
-                <td className="table-data"><img className="w-20 h-20 object-cover" src={learner.image || avatar} /></td>
+                <td className="table-data w-24"><img className="w-20 h-20 object-cover" src={learner.image || assets.avatar} /></td>
                 <td className="table-data">{learner.username}</td>
                 <td className="table-data">{learner.fullname}</td>
                 <td className="table-data">{learner.goal}</td>
                 <td className="table-data">{learner.email}</td>
+                <td className="table-data flex justify-center items-center">
+                  <div className="flex gap-4 h-20 w-20">
+                    {learner.github && <Link to={learner.github}><img src={assets.github} alt="GitHub Profile" /></Link> }
+                    {learner.linkedin && <Link to={learner.linkedin}><img src={assets.linkedin} alt="LinkedIn Profile" /></Link> }
+                  </div>
+                </td>
                 <td className="table-data">{learner.city}</td>
                 <td className="table-data">{learner.phone}</td>
                 <td className="table-data">{learner.bio}</td>
